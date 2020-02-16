7b05409d4d19d7afe366e329a4341b70435efe04
yuslianto <yuslianto56@gmail.com>
2020-02-16 19:58:44 +0700

    Make text input for email and password

--- src/Pages/AuthScreen/AuthScreen.js
+++ src/Pages/AuthScreen/AuthScreen.js
@@ -6,16 +6,43 @@ import {
     TouchableOpacity
 } from "react-native";
 
+import Input from '../../Molekul/Input/Input';
+
 const AuthScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
             <Text>Auth Screen</Text>
-            <TouchableOpacity
-            style={styles.btnConatiner}
-                onPress={()=>{navigation.navigate('Categories')}}
-            >
-                <Text style={styles.txtBtn}>Click Here</Text>
-            </TouchableOpacity>
+            <Input
+                id="email"
+                label="E-Mail"
+                keyboardType="email-address"
+                required
+                email
+                autoCapitalize="none"
+                errorText="Please enter a valid email address."
+                onInputChange={()=> {}}
+                initialValue=""
+            />
+            <Input
+                id="password"
+                label="Password"
+                keyboardType="default"
+                secureTextEntry
+                required
+                minLength={5}
+                autoCapitalize="none"
+                errorText="Please enter a valid password"
+                onInputChange={()=> {}}
+                initialValue=""
+            />
+            <View style={styles.btnConatiner}>
+                <TouchableOpacity
+                style={styles.btnConatiner}
+                    onPress={()=>{navigation.navigate('Categories')}}
+                >
+                    <Text style={styles.txtBtn}>Click Here</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
@@ -27,7 +54,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     btnConatiner: {
-        backgroundColor: 'blue'
+        backgroundColor: 'blue',
+        marginTop: 10,
     },
     txtBtn: {
         color: 'white'
